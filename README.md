46db7d54bf2d987c1bbb14c8dc3e01f5d2839a5a
ctorek <ctorek@protonmail.ch>
2020-06-30 10:43:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Enchantment Table
+
+literally just a discord bot that converts text into enchantment table language
+
+that's it that's the bot
---
+++ bot.rb
@@ -0,0 +1,42 @@
+require 'discordrb'
+
+# Enchantment table language
+translate = {
+    "a" => "ᔑ",
+    "b" => "ʖ",
+    "c" => "ᓵ",
+    "d" => "↸",
+    "e" => "ᒷ",
+    "f" => "⎓",
+    "g" => "⊣",
+    "h" => "⍑",
+    "i" => "╎",
+    "j" => "⋮",
+    "k" => "ꖌ",
+    "l" => "ꖎ",
+    "m" => "ᒲ",
+    "n" => "リ",
+    "o" => "𝙹",
+    "p" => "!¡",
+    "q" => "ᑑ",
+    "r" => "∷",
+    "s" => "ᓭ",
+    "t" => "ℸ",
+    "u" => "⚍",
+    "v" => "⍊",
+    "w" => "∴",
+    "x" => "/",
+    "y" => "||",
+    "z" => "⨅" 
+}
+
+bot = Discordrb::Bot.new token: 'token'
+puts "Invite this bot from #{bot.invite_url}"
+
+bot.message() do |event|
+    if event.message.content.split(' ')[0] == ".enchant"
+        event.respond event.message.content.split(' ')[1..-1].join('  ').gsub(/\w/, translate) 
+    end
+end
+
+bot.run
