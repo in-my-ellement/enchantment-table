0c5a96b48c5514bfb8aa097d012aca998ded82a7
ctorek <ctorek@protonmail.ch>
2020-06-30 15:11:13 -0400

    numbers don't break it anymore

--- bot.rb
+++ bot.rb
@@ -36,7 +36,7 @@ puts "Invite this bot from #{bot.invite_url}"
 bot.message() do |event|
     msg = event.message.content.split(' ')
     if msg[0] == ".enchant" && msg.length() >= 2
-        event.respond msg[1..-1].join('  ').gsub(/\w/, translate) 
+        event.respond msg[1..-1].join('  ').gsub(/[A-Za-z]/, translate) 
     end
 end
 
